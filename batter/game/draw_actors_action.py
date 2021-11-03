4d88363c5df63c629ae5e1c4a300fc232c9e248c
matiaserodriguez <maturod96@gmail.com>
2021-11-02 23:55:11 -0300

    draw actors comitted

--- batter/game/draw_actors_action.py
+++ batter/game/draw_actors_action.py
@@ -1,3 +1,13 @@
 from game.action import Action
 
-# TODO: Define the DrawActorsAction class here
+class DrawActorsAction(Action):
+
+    def __init__(self, output_service):
+        self._output_service = output_service
+
+    def execute(self, cast):
+        self._output_service.clear_screen()
+        for i in cast.keys():
+            actors = cast[i]
+            self._output_service.draw_actors(actors)
+        self._output_service.flush_buffer()
