fbd7dbf5c6b86af6d9e78c1071136e426b7bf5be
Scott-Thompson-MasterLink <Tho21034@byui.edu>
2021-11-02 20:30:27 -0600

    typo

--- batter/__main__.py
+++ batter/__main__.py
@@ -50,7 +50,7 @@ def main(screen):
     output_service = OutputService(screen)
     control_actors_action = ControlActorsAction(input_service)
     move_actors_action = MoveActorsAction()
-    handle_collisions_acition = HandleCollisionsAction()
+    handle_collisions_action = HandleCollisionsAction()
     draw_actors_action = DrawActorsAction(output_service)
     
     script["input"] = [control_actors_action]
