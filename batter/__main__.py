ba5cbeb937f0a7a80a637722c0cc8384c682e0c0
Scott-Thompson-MasterLink <Tho21034@byui.edu>
2021-11-04 22:15:50 -0600

    finished 2.0

--- batter/__main__.py
+++ batter/__main__.py
@@ -29,7 +29,7 @@ def main(screen):
         for y in range(2, 6):
             position = Point(x, y)
             brick = Actor()
-            brick.set_text("*")
+            brick.set_text("▪")
             brick.set_position(position)
             cast["brick"].append(brick)
 
